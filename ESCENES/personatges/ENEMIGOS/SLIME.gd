ff2c4357038fc13c2c48ee4f956e0e11f10123b1
LucaassFernandez <lucariopro333@gmail.com>
2024-04-19 14:33:01 +0200

    canvi

---
+++ ESCENES/personatges/ENEMIGOS/SLIME.gd
@@ -0,0 +1,14 @@
+extends AnimatableBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("run")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+
+	pass
