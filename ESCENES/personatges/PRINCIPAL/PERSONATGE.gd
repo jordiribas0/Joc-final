4a32b499afadb8d77da82906989dceac5ee704b6
LucaassFernandez <lucariopro333@gmail.com>
2024-04-19 13:53:36 +0200

    personage

--- ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
+++ ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
@@ -1,21 +1,11 @@
 extends CharacterBody2D
 
+var speed = 400
 
-const JUMP_VELOCITY = -400.0
-
-
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
 func _physics_process(delta):
-
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	get_input()
 	move_and_slide()
