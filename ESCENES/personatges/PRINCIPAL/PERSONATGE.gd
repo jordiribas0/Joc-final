c824a70744888da75c28cbc252c748fe001d558c
ArnauDachs <165768356+ArnauDachs@users.noreply.github.com>
2024-04-25 09:04:37 +0200

    a

--- ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
+++ ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
@@ -3,9 +3,8 @@ extends CharacterBody2D
 var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("left","right","up","down")
 	velocity = input_direction * speed
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
