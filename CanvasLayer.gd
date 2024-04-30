674396b107047363d58c6bf2575a104d178c0145
jordiribas0 <165767726+jordiribas0@users.noreply.github.com>
2024-04-30 22:13:22 +0200

    canvi

---
+++ CanvasLayer.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+@onready var menu : Node2D = $Menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_just_pressed("menu"):
+		get_tree().paused = true
+		menu.visible = get_tree().paused
+		
+func _on_quit_pressed():
+	get_tree().reload_current_scene()
