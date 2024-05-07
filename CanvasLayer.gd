d3d427f271d4b3abd608b465d6151949d3a0776a
jordiribas0 <165767726+jordiribas0@users.noreply.github.com>
2024-05-07 10:03:07 +0200

    patata

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
 @onready var menu : Node2D = $menu
 @onready var mapa : Node2D = $mapa
+@onready var controls : Node2D = $controls
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	menu.visible = false
 	mapa.visible = false
-
+	controls.visible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("menu"):
@@ -22,10 +23,20 @@ func _on_restart_pressed():
 
 func _on_resume_pressed():
 	get_tree().paused = false
-	menu.visible = get_tree().paused
-
+	menu.visible = false
 
 func _on_resume_m_pressed():
 	mapa.visible = false
 	
 	
+
+
+func _on_controls_pressed():
+	menu.visible = false
+
+	controls.visible = true
+
+
+func _on_salir_pressed():
+	controls.visible = false
+	menu.visible = true
