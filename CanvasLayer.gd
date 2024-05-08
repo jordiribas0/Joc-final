8d46b4eec80c3920439ba27e0196ac512102e39e
jordiribas0 <165767726+jordiribas0@users.noreply.github.com>
2024-05-08 23:32:30 +0200

    canvis

--- CanvasLayer.gd
+++ CanvasLayer.gd
@@ -2,11 +2,14 @@ extends CanvasLayer
 @onready var menu : Node2D = $menu
 @onready var mapa : Node2D = $mapa
 @onready var controls : Node2D = $controls
+@onready var audio : Node2D = $audio
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	inicia_audio()
 	menu.visible = false
 	mapa.visible = false
 	controls.visible = false
+	audio.visible = false
 	$claus.text = "Claus: " + str(get_parent().get_node("CharacterBody2D").num_claus)
 	$coins.text = "Coins: " + str(get_parent().get_node("CharacterBody2D").num_coins)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,3 +43,28 @@ func _on_controls_pressed():
 func _on_salir_pressed():
 	controls.visible = false
 	menu.visible = true
+	
+func _on_audio_pressed():
+	menu.visible = false
+	audio.visible = true
+
+func _on_sons_value_changed(value):
+	AudioServer.set_bus_volume_db(1, value - 100)
+	pass # Replace with function body.
+
+
+func _on_musica_value_changed(value):
+	AudioServer.set_bus_volume_db(2, value - 100)
+	pass # Replace with function body.
+
+
+func _on_salir_2_pressed():
+	audio.visible = false
+	menu.visible = true
+	pass # Replace with function body.
+	
+func inicia_audio():
+	for slider in audio.get_children():
+		if slider is HSlider:
+			slider.value = 90
+			
