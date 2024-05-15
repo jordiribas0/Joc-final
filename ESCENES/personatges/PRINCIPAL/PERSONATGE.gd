4ae05991d648c28c7c90980dc83d788c4ca9984b
jordiribas0 <165767726+jordiribas0@users.noreply.github.com>
2024-05-15 20:31:43 +0200

    trrr

--- ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
+++ ESCENES/personatges/PRINCIPAL/PERSONATGE.gd
@@ -67,3 +67,9 @@ func _on_player_hitbox_area_entered(area):
 	
 	if area.is_in_group("enemy"):
 		CorPle.vida -= 1
+
+
+func _on_teletrans_body_entered(body):
+	if body.get_name() == "CharacterBody2D":
+		body.global_position = Vector2(3143, 3762)
+	pass # Replace with function body.
